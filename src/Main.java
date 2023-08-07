bea0de0a6b41c8b494faf21cb8339e6c134989cc
Bianca Da Silva Alves <biancadsalves@gmail.com>
2023-08-07 08:59:42 -0300

    Fix indentation

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,22 @@ import classificacao.BubbleSort;
 import classificacao.InsertionSort;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BubbleSort bubble = new BubbleSort();
+  public static void main(String[] args) throws Exception {
+    BubbleSort bubble = new BubbleSort();
 
-        int array1[] = {4, 2, 3, 1};
-        bubble.sort(array1);
-        System.out.print("[BUBBLE SORT] Sorted array: ");
-        System.out.println(Arrays.toString(array1));
+    int array1[] = {4, 2, 3, 1};
+    bubble.sort(array1);
+    System.out.print("[BUBBLE SORT] Sorted array: ");
+    System.out.println(Arrays.toString(array1));
 
-        // ========================================================
+    // ========================================================
 
-        InsertionSort insert = new InsertionSort();
+    InsertionSort insert = new InsertionSort();
 
-        int array2[] = {11,7,9,10,5,6,2};
-        insert.sort(array2);
-        System.out.print("[INSERTION SORT] Sorted array: ");
-        System.out.println(Arrays.toString(array2));
+    int array2[] = {11,7,9,10,5,6,2};
+    insert.sort(array2);
+    System.out.print("[INSERTION SORT] Sorted array: ");
+    System.out.println(Arrays.toString(array2));
 
-    }
+  }
 }
--- src/classificacao/InsertionSort.java
+++ src/classificacao/InsertionSort.java
@@ -1,15 +1,15 @@
 package classificacao;
 
 public class InsertionSort {
-   public void sort(int[] array) {
-      for (int index=1; index<array.length; index++) {
+  public void sort(int[] array) {
+    for (int index=1; index<array.length; index++) {
 			int key = array[index]; // 'key' recebe o elemento a ser utilizado como chave nas comparações
 			int j = index - 1; // 'j' manipula o array, por meio de seus índices
 
 			/* Compare a chave com cada elemento à esquerda até que um
-			 * elemento menor que ele é encontrado.
-			 * Em ordem decrescente, altere key<array[j] para key>array[j].
-			 */
+			* elemento menor que ele é encontrado.
+			* Em ordem decrescente, altere key<array[j] para key>array[j].
+			*/
 			while (j >= 0 && key < array[j]) {
 				array[j + 1] = array[j];
 				--j;
@@ -17,6 +17,6 @@ public class InsertionSort {
 
 			// Atualize o array, colocando a chave depois do elemento menor que ele.
 			array[j + 1] = key;
-   	}	
-  	}
+  	}	
+  }
 }
